ed20c9b53d4c75f3674cc1f2085e9538cf9f14ca
Leon Magee <leonmagee@hotmail.com>
2013-03-22 13:02:03 -0700

    first file commit

---
+++ wp-cpt.php
@@ -0,0 +1,17 @@
+<?php
+
+function magee_media_cpts( $var1, $var2 ) {
+
+	if ( $var1 == true ) {
+
+		echo "this var is true " . $var2;
+
+	}		
+
+	else {
+		
+		echo "this var is false";
+	}
+}
+
+?>
